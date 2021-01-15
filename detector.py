9821e57e4743e3a2c769eff98216afde992f584c
shaungehring <shaungehring@me.com>
2021-01-15 12:42:17 -0500

    Added a detection log to coorelate the face back.

--- detector.py
+++ detector.py
@@ -7,11 +7,13 @@ import click
 import requests
 import re
 from pick import pick
+import csv
+import hashlib
 
 VIDEO_STORAGE = "videos"
 PROJECTS_FOLDER = "projects"
 VIDEO_PAGE = "https://www.tommycarstensen.com/terrorism/index.html"
-VIDEO_REGEX = r"\".*\.mp4"
+VIDEO_REGEX = r"\".*\.mp4" 
 
 
 def download_file(url, dl_location):
@@ -33,6 +35,14 @@ def project_selector():
     option, index = pick(options, title)
 
     return option
+
+def write_detection_log(project_folder, log):
+    with open(join(project_folder, 'face_detection.csv'), 'w', newline='') as csvfile:
+        cs_writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        cs_writer.writerow(['Face ID', 'Video ID', 'Timecode', 'Location'])
+
+        for item in log:
+            cs_writer.writerow(item)
 
 @click.group()
 def cli():
@@ -68,18 +78,19 @@ def download_sample_videos():
 
 @cli.command()
 def detect_faces():
-    project_folder = project_selector()
+    project_folder = "Test"# project_selector()
 
     face_front_cascade = cv2.CascadeClassifier('classifiers/haarcascade_frontalface_alt2.xml')
     face_profile_cascade = cv2.CascadeClassifier('classifiers/haarcascade_profileface.xml')
 
     video_files = listdir(join(PROJECTS_FOLDER, project_folder, VIDEO_STORAGE))
 
+    detection_log = []
+
     with click.progressbar(video_files, label="Video Files Processing", show_percent=True) as video_files_bar:
         for f in video_files_bar:
             if isfile(join(PROJECTS_FOLDER, project_folder, VIDEO_STORAGE, f)):
                 vidcap = cv2.VideoCapture(join(PROJECTS_FOLDER, project_folder, VIDEO_STORAGE, f))
-
                 count = 0
 
                 success, image = vidcap.read()
@@ -99,14 +110,18 @@ def detect_faces():
 
                         fr = 0
 
-                        for (x, y, w, h) in faces:
-                            try:
-                                # crop_face = gimg[y-10:y + h+10, x-10:x + w + 10]
-                                crop_face = image[y-10:y + h+10, x-10:x + w + 10]
-                                cv2.imwrite(f"{join(PROJECTS_FOLDER, project_folder)}/faces/{f}_frame{count}Person{fr}.jpg", crop_face)
-                                fr += 1
-                            except:
-                                pass
+                        if len(faces) > 0:
+                            for (x, y, w, h) in faces:
+                                try:
+                                    # crop_face = gimg[y-10:y + h+10, x-10:x + w + 10]
+                                    crop_face = image[y-10:y + h+10, x-10:x + w + 10]
+                                    face_id = hashlib.md5(f'{f}_{count}_{fr}'.encode('utf-8')).hexdigest()
+
+                                    cv2.imwrite(f"{join(PROJECTS_FOLDER, project_folder, 'faces')}/{face_id}.jpg", crop_face)
+                                    detection_log.append([face_id, f, (float(time)/fps), f'{y-10}, {y + h+10}, {x-10}, {x + w + 10}'])
+                                    fr += 1
+                                except:
+                                    pass
 
                     success, image = vidcap.read()
 
@@ -114,6 +129,17 @@ def detect_faces():
 
                 vidcap.release()
                 cv2.destroyAllWindows()
+
+        write_detection_log(join(PROJECTS_FOLDER, project_folder, 'faces'), detection_log)
+
+@cli.command()
+def find_people():
+    project_folder = project_selector()
+    faces_folder = join(PROJECTS_FOLDER, project_folder, 'faces')
+    people_folder = join(PROJECTS_FOLDER, project_folder, 'people')
+
+
+
 
 if __name__ == '__main__':
     cli()
